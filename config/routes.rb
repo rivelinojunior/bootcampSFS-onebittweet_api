cd0683954d10052031a0cedea162b4ea6e8006ca
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-11-21 17:40:56 -0400

    setup knock

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      post 'user_token', to: 'user_token#create'
+    end
+  end
 end
