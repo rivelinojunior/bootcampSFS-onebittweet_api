7e58cd085f898a717a9f62384f4846f446bb25d7
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-11-21 17:42:50 -0400

    generate model tweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_213629) do
+ActiveRecord::Schema.define(version: 2019_11_21_214136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2019_11_21_213629) do
     t.index ["followable_type", "followable_id"], name: "index_follows_on_followable_type_and_followable_id"
     t.index ["follower_id", "follower_type"], name: "fk_follows"
     t.index ["follower_type", "follower_id"], name: "index_follows_on_follower_type_and_follower_id"
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.integer "tweet_original_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +60,5 @@ ActiveRecord::Schema.define(version: 2019_11_21_213629) do
     t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
   end
 
+  add_foreign_key "tweets", "users"
 end
