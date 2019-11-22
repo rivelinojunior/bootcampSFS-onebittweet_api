5497464d312a819da087aad8458740a66075d872
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-11-22 12:50:45 -0400

    create helpers to test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Custom json helpers
+  config.include Requests::JsonHelpers, type: :request
+  # Custom Header helpers
+  config.include Requests::HeaderHelpers, type: :request
+  # Custom Serializer helpers
+  config.include Requests::SerializerHelpers, type: :request
 end
