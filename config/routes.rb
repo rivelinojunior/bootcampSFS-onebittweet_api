9eef58369bf9dae071a971c0b816f8eb1d1fba79
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-11-22 08:23:21 -0400

    create controller follow likes tweets users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'user_token', to: 'user_token#create'
+      resources :follows, only: %i[create destroy]
+      resources :likes, only: %i[create destroy]
+      resources :tweets, only: %i[index show create update destroy]
+      resources :users, only: %i[show create update destroy] do
+        member do
+          get 'following'
+          get 'followers'
+        end
+        get 'current', on: :collection
+      end
     end
   end
 end
