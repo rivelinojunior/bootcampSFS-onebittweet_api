6981dc64021929f11f0b50f9f2711f13bf113db3
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-11-22 10:02:09 -0400

    create serializers to user and tweet models

---
+++ app/serializers/api/v1/tweet_serializer.rb
@@ -0,0 +1,37 @@
+module Api
+  module V1
+    class TweetSerializer < ActiveModel::Serializer
+      attributes :id, :body, :tweet_original_id, :retweets_count, :likes_count,
+                 :liked
+      belongs_to :tweet_original
+      belongs_to :user
+
+      def tweet_original
+        return unless object.tweet_original
+
+        options = { each_serializer: Api::V1::TweetSerializer }
+        ActiveModelSerializers::SerializableResource.new(
+          object.tweet_original,
+          options
+        )
+      end
+
+      def user
+        options = { each_serializer: Api::V1::UserSerializer }
+        ActiveModelSerializers::SerializableResource.new(object.user, options)
+      end
+
+      def retweets_count
+        object.retweets.count
+      end
+
+      def likes_count
+        object.votes_for.size
+      end
+
+      def liked
+        object.liked_by current_user
+      end
+    end
+  end
+end
