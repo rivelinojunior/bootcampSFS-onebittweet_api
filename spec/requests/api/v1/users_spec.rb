d414de4e63fd58f12599c024037f1b854d446ec6
Rivelino junior <rv.fernandes.junior@gmail.com>
2019-11-24 10:47:31 -0400

    create test request to create and update user

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'rspec/json_expectations'
 
 RSpec.describe 'Api::V1::Users', type: :request do
   describe 'GET /api/v1/users/:id' do
@@ -130,6 +131,85 @@ RSpec.describe 'Api::V1::Users', type: :request do
         end
 
         it { expect(response).to have_http_status(:not_found) }
+      end
+    end
+  end
+
+  describe 'POST /api/v1/users' do
+    context 'Valid params' do
+      let(:user_params) { attributes_for(:user) }
+
+      it 'return created' do
+        post '/api/v1/users/', params: { user: user_params }
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'returns right user in json' do
+        post '/api/v1/users/', params: { user: user_params }
+        expect(json).to include_json(user_params.except(:password))
+      end
+
+      it 'create user' do
+        expect do
+          post '/api/v1/users/', params: { user: user_params }
+        end.to change { User.count }.by(1)
+      end
+    end
+
+    context 'Invalid params' do
+      let(:user_params) { { foo: :bar } }
+
+      before { post '/api/v1/users/', params: { user: user_params } }
+
+      it { expect(response).to have_http_status(:unprocessable_entity) }
+    end
+  end
+
+  describe 'PUT /api/v1/users/:id' do
+    context 'Unauthenticated' do
+      it_behaves_like :deny_without_authorization, :put, '/api/v1/users/-1'
+    end
+
+    context 'Authenticated' do
+      context 'Resource owner' do
+        context 'Valid params' do
+          let(:user) { create(:user) }
+          let(:user_params) { attributes_for(:user) }
+
+          before do
+            put "/api/v1/users/#{user.id}",
+                params: { user: user_params },
+                headers: header_with_authentication(user)
+          end
+
+          it { expect(response).to have_http_status(:success) }
+
+          it 'returns json with user updated' do
+            expect(json).to include_json(user_params.except(:password))
+          end
+        end
+
+        context 'Invalid params' do
+          let(:user_params) { {foo: :bar} }
+
+          before { post '/api/v1/users/', params: { user: user_params } }
+
+          it { expect(response).to have_http_status(:unprocessable_entity) }
+        end
+      end
+
+      context 'Not resource owner' do
+        let(:user) { create(:user) }
+        let(:other_user) { create(:user) }
+        let(:user_params) { attributes_for(:user) }
+
+        before do
+          put "/api/v1/users/#{other_user.id}",
+              params: {user: user_params },
+              headers: header_with_authentication(user)
+        end
+
+        it { expect(response).to have_http_status(:forbidden) }
       end
     end
   end
