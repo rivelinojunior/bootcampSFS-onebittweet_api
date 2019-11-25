b37273e5a9df6118950ce01ec4a9e4302cca8824
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-11-25 08:27:51 -0400

    create tweets controller

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,16 +1,58 @@
-class Api::V1::TweetsController < ApplicationController
-  def index
-  end
+module Api
+  module V1
+    class TweetsController < Api::V1::ApiController
+      before_action :set_tweet, except: %i[create index]
+      before_action :authenticate_user, except: [:show]
+      before_action :set_current_user
+      load_and_authorize_resource except: %i[index show create]
 
-  def show
-  end
+      def index
+        user = User.find params[:user_id]
+        @tweets = user.tweets.paginate(page: params[:page] || 1)
+        render json: @tweets
+      end
 
-  def create
-  end
+      def create
+        @tweet = Tweet.new tweet_params
 
-  def destroy
-  end
+        if @tweet.save
+          render json: @tweet, status: :created
+        else
+          render json: @tweet.errors, status: :unprocessable_entity
+        end
+      end
 
-  def update
+      def show
+        render json: @tweet
+      end
+
+      def destroy
+        @tweet.destroy
+      end
+
+      def update
+        if @tweet.update tweet_params
+          render json: @tweet
+        else
+          render json: @tweet.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def set_tweet
+        @tweet = Tweet.find(params[:id])
+      end
+
+      def tweet_params
+        params.require(:tweet)
+              .permit(:body, :tweet_original_id)
+              .merge(user: current_user)
+      end
+
+      def set_current_user
+        @current_user = current_user || nil
+      end
+    end
   end
 end
